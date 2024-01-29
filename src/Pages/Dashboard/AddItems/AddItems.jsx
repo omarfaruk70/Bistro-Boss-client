79b75d8a3ebb3262a36734af2dcddc478f01688b
omarfaruk70 <faruk1611281892@gmail.com>
2024-01-30 02:07:35 +0600

    added additem credientials

--- src/Pages/Dashboard/AddItems/AddItems.jsx
+++ src/Pages/Dashboard/AddItems/AddItems.jsx
@@ -1,12 +1,45 @@
 import { FaUtensils } from "react-icons/fa";
 import { useForm } from "react-hook-form";
 import Sectiontitle from "../../../components/SectionTitle/SectionTitle";
+import useAxiosPublic from "../../../hooks/useAxiosPublic";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import Swal from "sweetalert2";
 
+const IMG_HOSTING_API_KEY = import.meta.env.VITE_IMG_HOSTING_API_KEY;
+const Hosting_img_link = `https://api.imgbb.com/1/upload?key=${IMG_HOSTING_API_KEY}`;
 const AddItems = () => {
-  const { register, handleSubmit } = useForm();
+  const axiosPublic = useAxiosPublic();
+  const axiosSecure = useAxiosSecure();
+  const { register, handleSubmit, reset } = useForm();
 
-  const onSubmit = (data) => {
-    console.log(data);
+  const onSubmit = async (data) => {
+    const imageFile = { image: data.image[0] };
+     // kkhn o file type[img or anything] post korar jnno ekta header er vitor content-type: multipart/form-data set krte hy
+    const res = await axiosPublic.post(Hosting_img_link, imageFile, {
+      headers: {
+        "Content-Type": 'multipart/form-data'
+      }
+    });
+    if(res.data?.success){
+      const menuItem = {
+        name: data.name,
+        category: data.category,
+        price: parseFloat(data.price),
+        image: res.data.data.display_url,
+        recipe: data.recipe
+      }
+      const menuRes = await axiosSecure.post('/menu', menuItem);
+      reset();
+      if(menuRes.data.insertedId){
+        Swal.fire({
+          position: "top-end",
+          icon: "success",
+          title: "Your item is added",
+          showConfirmButton: false,
+          timer: 1500
+        });
+      }
+    }
   };
 
   return (
@@ -17,12 +50,12 @@ const AddItems = () => {
       ></Sectiontitle>
       <div>
         <form onSubmit={handleSubmit(onSubmit)}>
-          <label  className="form-control w-full">
+          <label className="form-control w-full">
             <div className="label">
               <span className="label-text">Recipe name*</span>
             </div>
             <input
-            {...register("name", {required: true})}
+              {...register("name", { required: true })}
               type="text"
               placeholder="Recipe Name"
               className="input input-bordered w-full"
@@ -30,18 +63,16 @@ const AddItems = () => {
           </label>
           <div className="flex gap-6">
             {/* category */}
-            <label
-              className="form-control w-full my-6"
-            >
+            <label className="form-control w-full my-6">
               <div className="label">
                 <span className="label-text">Category*</span>
               </div>
               <select
-              defaultValue={'defaultValue'}
-               {...register("category", {required: true})}
+                defaultValue={"defaultValue"}
+                {...register("category", { required: true })}
                 className="select select-bordered w-full"
               >
-                <option disabled value={'defaultValue'}>
+                <option disabled value={"defaultValue"}>
                   Category
                 </option>
                 <option value={"salad"}>Salad</option>
@@ -52,28 +83,38 @@ const AddItems = () => {
               </select>
             </label>
             {/* price */}
-            <label  className="form-control w-full my-6">
+            <label className="form-control w-full my-6">
               <div className="label">
                 <span className="label-text">Price*</span>
               </div>
               <input
-              {...register("price", {required: true})}
+                {...register("price", { required: true })}
                 type="number"
                 placeholder="Recipe price"
                 className="input input-bordered w-full"
               />
             </label>
           </div>
-            <label className="form-control w-full">
+          <label className="form-control w-full">
             <div className="label">
               <span className="label-text">Recipe details*</span>
             </div>
-            <textarea  {...register("recipe", {required: true})} className="textarea textarea-bordered" placeholder="Recipe details"></textarea>
+            <textarea
+              {...register("recipe", { required: true })}
+              className="textarea textarea-bordered"
+              placeholder="Recipe details"
+            ></textarea>
           </label>
           <div className="form-control w-full my-3">
-          <input  {...register("image", {required: true})} type="file" className="file-input w-full max-w-xs" />
+            <input
+              {...register("image", { required: true })}
+              type="file"
+              className="file-input w-full max-w-xs"
+            />
           </div>
-            <button className="btn bg-yellow-400 text-white">Add Item <FaUtensils className="text-xl ml-2"></FaUtensils></button>
+          <button className="btn bg-yellow-400 text-white">
+            Add Item <FaUtensils className="text-xl ml-2"></FaUtensils>
+          </button>
         </form>
       </div>
     </div>
